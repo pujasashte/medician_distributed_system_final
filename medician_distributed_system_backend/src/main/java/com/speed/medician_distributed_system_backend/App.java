a038a3bc72fbf5a823e38919548d91b2e45eec7f
puja <pujasashte123@gmail.com>
2020-02-08 13:35:00 +0530

    My project setup Completed sucessfully

---
+++ medician_distributed_system_backend/src/main/java/com/speed/medician_distributed_system_backend/App.java
@@ -0,0 +1,13 @@
+package com.speed.medician_distributed_system_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
